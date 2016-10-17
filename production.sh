89b1622fde730562a79e660bf7fb805122bb792d
ian <xiongbo027@gmail.com>
2016-10-17 17:17:32 +0800

    Update production.sh

--- production.sh
+++ production.sh
@@ -42,6 +42,14 @@ if [ $dorm = Y ]; then
   sudo apt-get -y install postgresql-client libpq-dev
 fi
 
+echo "-----Install Others-----"
+echo "Well you need the gem of curb?(Y/N) (default: N) __"
+read dorm
+dorm=${dorm:=N}
+if [ $dorm = Y ]; then
+  sudo apt-get -y install libcurl3 libcurl3-gnutls libcurl4-openssl-dev
+fi
+
 echo "-----Install ruby by rbenv-----"
 git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
@@ -50,7 +58,7 @@ git clone https://github.com/rkh/rbenv-update.git ~/.rbenv/plugins/rbenv-update
 echo -e 'export PATH="$HOME/.rbenv/bin:$PATH"\n' >> ~/.bashrc
 echo -e 'eval "$(rbenv init - --no-rehash)"' >> ~/.bashrc
 sudo chown -R deploy:deploy ~/.rbenv/
-. ./bashrc
+. ./.bashrc
 
 ruby_version="2.3.0"
 printf "Installing Ruby $ruby_version ..."
